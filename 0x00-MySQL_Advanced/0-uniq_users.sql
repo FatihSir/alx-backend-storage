74d5ffa665312bdced52b4ff1f140fef8bf5dfef
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-17 22:28:22 +0300

    fixes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,4 @@
--- Creates a table 'users' with the following attributes:
--- id: integer, never null, auto-increment, and set as the primary key
--- email: string (255 characters), never null, and unique
--- name: string (255 characters), can be null (default NULL)
--- If the table already exists, the script
--- will not fail and can be executed on any database
+-- Creates a table 'users'
 
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,20 +5,20 @@
 -- Creates a new 'orders' table with attributes:
 --   - item_name: string (255 characters), never null
 --   - number: integer, never null
--- Inserts some initial data into the 'items' table: apple, pineapple, pear
+-- Inserts some initial data into the 'items'
+-- table: apple, pineapple, pear
 
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE IF NOT EXISTS items (
     name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL DEFAULT 10
+    quantity int NOT NULL DEFAULT 10
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     item_name VARCHAR(255) NOT NULL,
-    number INT NOT NULL
+    number int NOT NULL
 );
 
--- Insert initial data into 'items' table
 INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
