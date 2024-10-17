9b0bcfa4de81942b1f44dd367923781bf5e785d6
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-17 22:44:10 +0300

    more fixes

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,13 +1,12 @@
 -- Procedure to add a new correction for a student, creating a project if it doesn't exist
+
 DELIMITER $$;
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    -- Check if project exists, create it if not
     IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name) THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
     
-    -- Insert correction for the student
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END$$
